cee5062498259e235d96172d342237a79982bb75
Grace Zhang <gracewzhang01@gmail.com>
2022-02-24 09:09:27 -0600

    Start hamburger menu

---
+++ zubhub_frontend/zubhub/src/components/hamburger/Hamburger.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Hamburger(props) {
+    return (
+        <Button>
+            <MenuIcon/>
+        </Button>
+    );
+}
+
+export default Hamburger;
